30e13dbeaba1dfcb85517da214fa2b8aa30a227c
IshanX111 <51012063+IshanX111@users.noreply.github.com>
2020-01-02 23:28:50 +0600

    Add files via upload

---
+++ servant.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t,a;
+    cin>>t;
+    while(t--){
+        cin>>a;
+        if(a<10){
+            printf("What an obedient servant you are!\n");
+
+        }
+        else
+            printf("-1\n");
+
+    }
+
+}
