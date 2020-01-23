d0dbcac01f95ed813185f76c358fb4d822c7b891
IshanX111 <51012063+IshanX111@users.noreply.github.com>
2020-01-24 02:15:56 +0600

    Add files via upload

---
+++ Decrement.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int n;
+    cin>>n;
+    if(n%4!=0){
+        printf("%d\n",n-1);
+    }
+    else
+        printf("%d\n",n+1);
+
+
+}
+
