6000379c0b91829c3da7bf093cd48b89f6dd0f30
IshanX111 <51012063+IshanX111@users.noreply.github.com>
2020-02-01 00:48:46 +0600

    Add files via upload

---
+++ SumorDifference.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int n1,n2;
+    cin>>n1>>n2;
+    if(n1>n2){
+        printf("%d\n",n1-n2);
+    }
+    else
+        printf("%d\n",n1+n2);
+
+}
