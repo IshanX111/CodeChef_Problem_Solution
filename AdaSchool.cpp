f16140bf9c45105cc5e2539f56fcb8f25a1ec086
IshanX111 <51012063+IshanX111@users.noreply.github.com>
2020-02-12 01:11:11 +0600

    Add files via upload

---
+++ AdaSchool.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t,m,n;
+    cin>>t;
+    while(t--){
+        cin>>m>>n;
+        if(m%2==0 || n%2==0){
+            printf("YES\n");
+        }
+        else printf("NO\n");
+    }
+
+
+
+}
